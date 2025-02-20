22b9248f99ae378a13605a9feeb6ed7abfa3cf53
Muqeet Suhail <muqeetsuhail1234@gmail.com>
2025-02-20 22:48:13 +0500

    "Initialized a simple rust display program"

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!(
+        "\n🔧 Welcome to Rust Codespace Setup!🔧\n\
+        ---------------------------------------\n\
+        This program demonstrates a simple Rust Codespace configuration using Dev Containers.\n"
+    );
+    println!();
+}
